a905ed81f4285c186fef83c08e63d759278acf7e
gabe <gabe@delgadobunch.com>
2022-03-17 13:24:19 -0400

    getting more comfortable with flask with mongoengine, added test model, url params, query params, form submission

--- server.py
+++ server.py
@@ -1,13 +1,41 @@
-from flask import Flask
+from flask import Flask, request
 from pymongo import MongoClient
+from flask_mongoengine import MongoEngine
+from models import User
 
 app = Flask(__name__)
 client = MongoClient("localhost", 27017)
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'testflaskdb',
+    'host': 'localhost',
+    'port': 27017
+}
+
+db = MongoEngine(app)
 
 
-@app.route('/')
-def home():
-    return "hi there"
+@app.route('/<stuff>/<otherstuff>', methods=["GET", "POST"])
+def home(stuff, otherstuff):  # for normal url params do like this line and line above, and use as normal variable
+
+    if request.method == "GET":
+
+        # print(request.ars)
+        print(otherstuff)
+        return stuff
+
+    if request.method == "POST":
+        # for post form submission use request.form
+        print(request.form['username'])
+        print(request.form['password'])
+        return "you did a post"
+
+
+@app.route('/', methods=["GET"])
+def otherhome():
+    if request.method == "GET":
+        User(name='laura', email='poop@gmail.com').save()
+        # print(request.args["arg"])  # for query params use request.args
+        return "i print"
 
 
 if __name__ == "__main__":
