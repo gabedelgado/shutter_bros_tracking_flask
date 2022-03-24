abd8287cc17f8e8eb35c23ca8bdfe8fb4538df13
gabe <gabe@delgadobunch.com>
2022-03-24 15:32:58 -0400

    updated requirements, removed unnecessary test file, cleaned up comments and main server.py file

--- server.py
+++ server.py
@@ -2,26 +2,16 @@ from flask import Flask, request
 from pymongo import MongoClient
 from flask_mongoengine import MongoEngine
 from models import User
-from moreRoutes import testpage
 from routes.orderRoutes import order
 from routes.authRoutes import auth
-# add method =[get post] params to all routes
-
-# TODO:
-# move jwt stuff back to routes, keep only db stuff inside model methods
-# get all orders
-# get one order
-# make random 10 digit code for tracking
-# create order
-
 
 app = Flask(__name__)
 
-# separate routes into different files like this, add url prefix like this.
-app.register_blueprint(testpage, url_prefix="/test")
+# separate routes into different blueprints
 app.register_blueprint(order, url_prefix="/api/order")
 app.register_blueprint(auth, url_prefix="/api/auth")
-# connect to mongodb like this
+
+# Connecting to mongodb
 client = MongoClient("localhost", 27017)
 app.config['MONGODB_SETTINGS'] = {
     'db': 'shutterBrosTracking',
@@ -29,32 +19,8 @@ app.config['MONGODB_SETTINGS'] = {
     'port': 27017
 }
 
+# setup mongoengine for easier access/manipulation of mongo db
 db = MongoEngine(app)
-
-
-# @app.route('/<stuff>/<otherstuff>', methods=["GET", "POST"])
-# def home(stuff, otherstuff):  # for normal url params do like this line and line above, and use as normal variable
-
-#     if request.method == "GET":
-
-#         # print(request.ars)
-#         print(otherstuff)
-#         return stuff
-
-#     if request.method == "POST":
-#         # for post form submission use request.form
-#         print(request.form['username'])
-#         print(request.form['password'])
-#         return "you did a post"
-
-
-# @app.route('/', methods=["GET"])
-# def otherhome():
-#     if request.method == "GET":
-#         User(name='laura', email='poop@gmail.com').save()
-#         # print(request.args["arg"])  # for query params use request.args
-#         return "i print"
-
 
 if __name__ == "__main__":
     app.run()
