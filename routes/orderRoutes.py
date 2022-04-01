d5d990aacf634a6f98a31b2953247325279cda32
gabe <gabe@delgadobunch.com>
2022-04-01 10:44:17 -0400

    changed newOrder route to see whats coming from webhook

--- routes/orderRoutes.py
+++ routes/orderRoutes.py
@@ -21,22 +21,29 @@ def getAll():
 @order.route("/newOrder", methods=["POST"])
 def newOrder():
     if request.method == "POST":
-        customername = request.form["customerName"]
-        jobaddress = request.form["address"]
-        # create random 10 digit code
-        ordernumber = ''.join(random.choices(
-            string.ascii_uppercase + string.digits, k=10))
+        print("REQUEST ******************************************")
+        print(request)
+        print("REQUEST.FORM ***************************")
+        print(request.form)
+        print("REQUEST.FORM KEYS ***************************")
+        print(request.form.keys())
+        return {"stuff": "stuff"}, 200
+        # customername = request.form["customerName"]
+        # jobaddress = request.form["address"]
+        # # create random 10 digit code
+        # ordernumber = ''.join(random.choices(
+        #     string.ascii_uppercase + string.digits, k=10))
 
-        while Order.objects(orderNumber=ordernumber):
-            ordernumber = ''.join(random.choices(
-                string.ascii_uppercase + string.digits, k=10))
+        # while Order.objects(orderNumber=ordernumber):
+        #     ordernumber = ''.join(random.choices(
+        #         string.ascii_uppercase + string.digits, k=10))
 
-        neworder = Order(orderNumber=ordernumber,
-                         customerName=customername, jobAddress=jobaddress).save()
-        return jsonify(neworder)
+        # neworder = Order(orderNumber=ordernumber,
+        #                  customerName=customername, jobAddress=jobaddress).save()
+        # return jsonify(neworder)
 
-        # how can i secure this to only being called from the leaptodigital webhook
-        return "only leap to digital web hook allowed to access this, create new post in db, send status back"
+        # # how can i secure this to only being called from the leaptodigital webhook
+        # return "only leap to digital web hook allowed to access this, create new post in db, send status back"
 
 
 # retrieve specific order
